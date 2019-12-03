e771860b6d5997630cabc1c956f3bee1a0746b59
nadyrbek97 <nadyrbek97@gmail.com>
2019-12-03 19:04:20 +0600

    create services application, model

---
+++ services/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from departament.models import Departament
+
+
+class Service(models.Model):
+    title = models.CharField(max_length=500)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    description = models.TextField()
+    department = models.ForeignKey(Departament, on_delete=models.CASCADE,
+                                   related_name="services")
+
+    class Meta:
+        verbose_name = "Service"
+        verbose_name_plural = "Services"
+
+    def __str__(self):
+        return f"{self.title}"
