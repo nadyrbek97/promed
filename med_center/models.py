a09ab95097b809b1e14ac8e02ec08af47af67bde
nadyrbek97 <nadyrbek97@gmail.com>
2019-12-04 19:14:07 +0600

    add schedule time for med center

--- med_center/models.py
+++ med_center/models.py
@@ -26,3 +26,19 @@ class MedCenterPhoto(models.Model):
 
     def __str__(self):
         return f"{self.title}"
+
+
+class ScheduleTime(models.Model):
+    med_center = models.ForeignKey(MedicalCenter, on_delete=models.CASCADE,
+                                   related_name="schedule_times")
+    title = models.CharField(max_length=30)
+    start_time = models.TimeField(null=True, blank=True)
+    end_time = models.TimeField(null=True, blank=True)
+    is_weekend = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Schedule Time"
+        verbose_name_plural = "Schedule Times"
+
+    def __str__(self):
+        return f"{self.title}  start: {self.start_time}  end; {self.end_time}"
--- med_center/views.py
+++ med_center/views.py
@@ -7,8 +7,11 @@ from profiles.models import Doctor
 
 def main_page_view(request):
 
+    # med center
     med_center = MedicalCenter.objects.get(id=1)
     logo = med_center.photos.filter(is_logo=True).first()
+    schedule_times = med_center.schedule_times.all()
+
     main_branch = Branch.objects.get(med_center_id=med_center.id)
     main_branch_number = main_branch.numbers.first()
     doctors = Doctor.objects.all()
@@ -16,6 +19,8 @@ def main_page_view(request):
     context = {
         "med_center": med_center,
         "logo": logo,
+        "schedules": schedule_times,
+
         "main_branch": main_branch,
         "main_branch_number": main_branch_number.number,
         "doctors": doctors
