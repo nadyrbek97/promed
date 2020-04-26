438033fb3c2db4dacf7fde233db74027d3c14e73
Nadyrbek <nadyrbek97@gmail.com>
2020-04-27 03:10:44 +0600

    add api statistics to doctor-profile page

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from api.views import ChartData
+from api.views import VisitsByServiceData, VisitsForLastWeekData
 
 urlpatterns = [
-    path('chart/data/', ChartData.as_view(), name="chart-data-view"),
+    path('data/visits-by-service/', VisitsByServiceData.as_view(), name="visits-by-service-data-view"),
+    path('data/visits-for-last-week/', VisitsForLastWeekData.as_view(), name="visits-for-last-week-data-view"),
 ]
--- api/views.py
+++ api/views.py
@@ -4,18 +4,34 @@ from rest_framework.response import Response
 from profiles.models import User
 
 
-class ChartData(APIView):
+class VisitsForLastWeekData(APIView):
     authentication_classes = []
     permission_classes = []
 
     # Number of visits in last week
     def get(self, request, format=None):
-        users_count = User.objects.all().count()
-        labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
-        default_items = [users_count, 33, 12, 43, 51, 31]
+        labels = ['21-04-2020', '22-04-2020', '23-04-2020', '24-04-2020', '25-04-2020', '26-04-2020', '27-04-2020']
+        default_items = [23, 33, 12, 43, 51, 31, 65]
 
         data = {
             "labels": labels,
-            "default" : default_items
+            "default": default_items
+        }
+        return Response(data)
+
+
+class VisitsByServiceData(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    # Number of visits in last week
+    def get(self, request, format=None):
+        labels = ['Узи Печени', 'Узи Всех Органов', 'Узи Нижних Конечностей',
+                  'Узи селезенки', 'Узи Почек', 'Узи Брюшной Полости']
+        default_items = [32, 33, 12, 43, 51, 31]
+
+        data = {
+            "labels": labels,
+            "default": default_items
         }
         return Response(data)
