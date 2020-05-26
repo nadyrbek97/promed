8854c78d5d3dafa0bd362c92fd130b727cc1375c
Nadyrbek <nadyrbek97@gmail.com>
2020-05-27 00:07:36 +0600

    create api for department detail page

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from services.models import Service
+from department.models import Department
 
 
 class ServiceSerializer(serializers.ModelSerializer):
@@ -8,3 +9,10 @@ class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
         exclude = ['department']
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Department
+        exclude = ['med_center']
